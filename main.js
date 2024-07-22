1657fe71e493a0de0d25332621f1405ee6f6e3c5
DiamondStalker <cmoreno981@gmail.com>
2024-07-22 00:47:34 -0500

    Se mejora la interfaz, se creo un sistema de outputs dentro del sistema

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const exec = require('child_process').exec;
 const os = require('os');
-const fs = require('fs'); // Para verificar si la carpeta existe
+const fs = require('fs');
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
-        width: 420,
+        width: 500,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
@@ -14,60 +14,56 @@ function createWindow() {
             enableRemoteModule: false,
             nodeIntegration: false,
         },
-        frame: false, // Desactiva la barra de título y los bordes del sistema
+        frame: false,
     });
 
     mainWindow.loadFile('index.html');
 
+    const sendOutput = (output) => {
+        mainWindow.webContents.send('command-output', { output });
+    };
+
+    sendOutput('Validando Existencia de Node\n');
+
     // Verificar la instalación de Node.js
-    console.log('Verificando la instalación de Node.js...');
-    exec('node -v', (error, stdout, stderr) => {
+    exec('node -v', (error, stdout) => {
         if (error) {
-            console.error('Node.js no está instalado.');
             mainWindow.webContents.send('node-status', { status: 'not-installed' });
+            sendOutput('Node no se encuentra instalado \n');
         } else {
             const version = stdout.trim();
-            console.log('Node.js está instalado:', version);
             mainWindow.webContents.send('node-status', { status: 'installed', version: version });
+            sendOutput('Node se encuentra instalado \n');
         }
     });
 
-    // Verificar si el repositorio ya está descargado
+    // Verificar si el repositorio ya existe
     const documentsPath = path.join(os.homedir(), 'Documents');
-    const repoName = 'Automatizacion'; // Nombre de la carpeta del repositorio
-    const clonePath = path.join(documentsPath, repoName);
+    const repoName = 'Automatizacion';
+    const repoPath = path.join(documentsPath, repoName);
 
-    fs.access(clonePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            // Carpeta no existe
-            mainWindow.webContents.send('repo-status', { status: 'not-exists' });
-        } else {
-            // Carpeta existe
-            mainWindow.webContents.send('repo-status', { status: 'exists' });
-        }
-    });
+    if (fs.existsSync(repoPath)) {
+        mainWindow.webContents.send('repo-status', { status: 'exists' });
+    } else {
+        mainWindow.webContents.send('repo-status', { status: 'not-exists' });
+    }
 
     // Escuchar el evento de instalación de Node.js
     ipcMain.on('install-node', () => {
-        console.log('Iniciando instalación de Node.js...');
         exec(`
             echo Node.js no está instalado. Instalando...
             powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
             echo Configuración de Chocolatey completada. Instalando Node.js LTS...
             choco install nodejs-lts --version=20.15.1 -y
-        `, (error, stdout, stderr) => {
+        `, (error, stdout) => {
             if (error) {
-                console.error('Error instalando Node.js:', error);
+                sendOutput('Error instalando Node.js:', error);
             } else {
-                console.log('Node.js instalado correctamente:', stdout);
-                // Verificar la instalación de Node.js nuevamente
-                exec('node -v', (error, stdout, stderr) => {
+                exec('node -v', (error, stdout) => {
                     if (error) {
-                        console.error('Node.js no está instalado.');
                         mainWindow.webContents.send('node-status', { status: 'not-installed' });
                     } else {
                         const version = stdout.trim();
-                        console.log('Node.js está instalado:', version);
                         mainWindow.webContents.send('node-status', { status: 'installed', version: version });
                     }
                 });
@@ -77,50 +73,77 @@ function createWindow() {
 
     // Escuchar el evento de descarga de repositorio
     ipcMain.on('download-repo', () => {
-        console.log('Descargando repositorio...');
-
         const repoUrl = 'https://github.com/DiamondStalker/Automatizacion-Rudy'; // Reemplaza con la URL del repositorio que deseas clonar
-        const clonePath = path.join(documentsPath, repoName);
+        const cloneCommand = `git clone ${repoUrl} "${repoPath}"`;
 
-        if (fs.existsSync(clonePath)) {
-            console.log('Repositorio ya existe.');
-            mainWindow.webContents.send('download-status', { status: 'exists', message: 'El repositorio ya está descargado.' });
-        } else {
-            const cloneCommand = `git clone ${repoUrl} "${clonePath}"`;
+        exec(cloneCommand, (error, stdout) => {
+            if (error) {
+                sendOutput('Error descargando el repositorio:', error);
+                mainWindow.webContents.send('download-status', { status: 'error', message: error.message });
+            } else {
+                sendOutput('Repositorio descargado correctamente:', stdout);
+                mainWindow.webContents.send('download-status', { status: 'success', message: 'Repositorio descargado correctamente.' });
+                mainWindow.webContents.send('repo-status', { status: 'exists' });
+            }
+        });
+    });
 
-            exec(cloneCommand, (error, stdout, stderr) => {
+    // Escuchar el evento de verificación de node_modules
+    ipcMain.on('check-node-modules', () => {
+        const documentsPath = path.join(os.homedir(), 'Documents');
+        const repoName = 'Automatizacion';
+        const repoPath = path.join(documentsPath, repoName);
+        const nodeModulesPath = path.join(repoPath, 'node_modules');
+    
+        // const sendOutput = (output) => {
+        //     mainWindow.webContents.send('command-output', { output });
+        // };
+    
+        if (!fs.existsSync(nodeModulesPath)) {
+            exec(`cd "${repoPath}" && npm install`, (error, stdout, stderr) => {
+                sendOutput(stdout);
                 if (error) {
-                    console.error('Error descargando el repositorio:', error);
-                    mainWindow.webContents.send('download-status', { status: 'error', message: error.message });
-                } else {
-                    console.log('Repositorio descargado correctamente:', stdout);
-                    mainWindow.webContents.send('download-status', { status: 'success', message: 'Repositorio descargado correctamente.' });
-                    // Actualiza el estado del repositorio después de la descarga
-                    mainWindow.webContents.send('repo-status', { status: 'exists' });
+                    sendOutput(`Error ejecutando npm install: ${error.message}`);
                 }
+                sendOutput(stderr);
+                
+                exec(`cd "${repoPath}" && npm run start`, (error, stdout, stderr) => {
+                    sendOutput(stdout);
+                    if (error) {
+                        sendOutput(`Error ejecutando npm run start: ${error.message}`);
+                    }
+                    sendOutput(stderr);
+                });
+            });
+        } else {
+            exec(`cd "${repoPath}" && npm run start`, (error, stdout, stderr) => {
+                sendOutput(stdout);
+                if (error) {
+                    sendOutput(`Error ejecutando npm run start: ${error.message}`);
+                }
+                sendOutput(stderr);
             });
         }
     });
+    
+    
 
     // Escuchar el evento de cierre de la aplicación
     ipcMain.on('close-app', () => {
-        console.log('Cerrando la aplicación...');
         app.quit();
     });
 }
 
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-});
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
     }
 });
--- preload.js
+++ preload.js
@@ -4,7 +4,9 @@ contextBridge.exposeInMainWorld('electron', {
     receiveNodeStatus: (callback) => ipcRenderer.on('node-status', callback),
     receiveRepoStatus: (callback) => ipcRenderer.on('repo-status', callback),
     receiveDownloadStatus: (callback) => ipcRenderer.on('download-status', callback),
+    receiveCommandOutput: (callback) => ipcRenderer.on('command-output', callback),
     installNode: () => ipcRenderer.send('install-node'),
     downloadRepo: () => ipcRenderer.send('download-repo'),
+    checkNodeModules: () => ipcRenderer.send('check-node-modules'),
     closeApp: () => ipcRenderer.send('close-app')
 });
